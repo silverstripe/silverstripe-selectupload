c3e2e6aa0f4f3775609d5cb4ef0488b37b2e82e1
Scott Sutherland <ssutherland@silverstripe.com>
2018-12-04 10:08:44 +1300

    added use statements

--- src/SelectHtmlEditorFieldExtension.php
+++ src/SelectHtmlEditorFieldExtension.php
@@ -2,35 +2,42 @@
 
 namespace SilverStripe\SelectUpload;
 
-class SelectHtmlEditorFieldExtension extends Extension {
+use SilverStripe\Core\Extension;
+use SilverStripe\SelectUpload\FolderDropdownField;
+use SilverStripe\SelectUpload\SelectUploadField
 
-	/**
-	 * Make the "from cms" folder use the saved folder
-	 *
-	 * @param int $parentID
-	 */
-	public function updateAttachParentID(&$parentID) {
-		// If given assume that a folder has been posted
-		if($parentID) {
-			FolderDropdownField::set_last_folder($parentID);
-		} else {
-			$parentID = FolderDropdownField::get_last_folder();
-		}
-	}
+class SelectHtmlEditorFieldExtension extends Extension
+{
 
-	/**
-	 * Substitute the SelectUploadField in place of the UploadField
-	 *
-	 * @param Form $form
-	 */
-	public function updateMediaForm($form) {
-		$computerUploadField = SelectUploadField::create('AssetUploadField', '')
-			->setPreviewMaxWidth(40)
-			->setPreviewMaxHeight(30)
-			->addExtraClass('ss-assetuploadfield ss-selectassetuploadfield')
-			->removeExtraClass('ss-uploadfield ss-selectuploadfield')
-			->setTemplate('HtmlEditorField_SelectUploadField')
-			->setForm($form);
-		$form->Fields()->replaceField('AssetUploadField', $computerUploadField);
-	}
+    /**
+     * Make the "from cms" folder use the saved folder
+     *
+     * @param int $parentID
+     */
+    public function updateAttachParentID(&$parentID)
+    {
+        // If given assume that a folder has been posted
+        if ($parentID) {
+            FolderDropdownField::set_last_folder($parentID);
+        } else {
+            $parentID = FolderDropdownField::get_last_folder();
+        }
+    }
+
+    /**
+     * Substitute the SelectUploadField in place of the UploadField
+     *
+     * @param Form $form
+     */
+    public function updateMediaForm($form)
+    {
+        $computerUploadField = SelectUploadField::create('AssetUploadField', '')
+            ->setPreviewMaxWidth(40)
+            ->setPreviewMaxHeight(30)
+            ->addExtraClass('ss-assetuploadfield ss-selectassetuploadfield')
+            ->removeExtraClass('ss-uploadfield ss-selectuploadfield')
+            ->setTemplate('HtmlEditorField_SelectUploadField')
+            ->setForm($form);
+        $form->Fields()->replaceField('AssetUploadField', $computerUploadField);
+    }
 }
